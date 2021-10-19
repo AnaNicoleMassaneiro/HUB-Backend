f7b34c335745eec4844e9e8cdf0dd5b8a46a4300
sauxthem <matt.rock4321@hotmail.com>
2021-10-19 19:57:13 -0300

    Change product requests for new endpoints

---
+++ HubUfpr.NetCore/Requests/SearchProductByName.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace HubUfpr.API.Requests
+{
+    public class SearchProductByName
+    {
+
+        [JsonProperty(PropertyName = "name", NullValueHandling = NullValueHandling.Include)]
+        public string Name { get; set; }
+
+        [JsonProperty(PropertyName = "ReturnActiveOnly", NullValueHandling = NullValueHandling.Include)]
+        public bool ReturnActiveOnly { get; set; }
+
+    }
+}
