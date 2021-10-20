11c930efc096e9c8876124ca1a3e4b0688d9d4e2
sauxthem <matt.rock4321@hotmail.com>
2021-10-20 20:48:29 -0300

    Alter parameter order for improved usability

--- HubUfpr.NetCore/Requests/SearchVendedorByLocation.cs
+++ HubUfpr.NetCore/Requests/SearchVendedorByLocation.cs
@@ -4,10 +4,10 @@ namespace HubUfpr.API.Requests
 {
     public class SearchVendedorByLocation
     {
-        [JsonProperty(PropertyName = "longitude", NullValueHandling = NullValueHandling.Ignore)]
-        public float Longitude { get; set; }
-
         [JsonProperty(PropertyName = "latitude", NullValueHandling = NullValueHandling.Ignore)]
         public float Latitude { get; set; }
+
+        [JsonProperty(PropertyName = "longitude", NullValueHandling = NullValueHandling.Ignore)]
+        public float Longitude { get; set; }
     }
 }
